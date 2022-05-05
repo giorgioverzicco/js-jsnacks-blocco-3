bde1a3cab2413e4156d464b72cce1d65cea3bf3b
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-05 12:58:00 +0200

    add break

--- snack-1/main.js
+++ snack-1/main.js
@@ -3,8 +3,9 @@ const arr = [];
 let sum = 0;
 while (sum < 50) {
   const userInput = Number(prompt("Inserisci un numero:"));
-  if (isNaN(userInput)) continue;
+  if (isNaN(userInput) || userInput >= 50) continue;
   sum += userInput;
+  if (sum >= 50) break;
   arr.push(userInput);
 }
 console.log(arr);
