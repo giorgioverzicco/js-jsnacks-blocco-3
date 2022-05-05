025694776172d4268b19f8e8c3877847860c0ec6
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-05 12:21:06 +0200

    add isNaN check

--- snack-1/main.js
+++ snack-1/main.js
@@ -3,6 +3,7 @@ const arr = [];
 let sum = 0;
 while (sum < 50) {
   const userInput = Number(prompt("Inserisci un numero:"));
+  if (isNaN(userInput)) continue;
   sum += userInput;
   arr.push(userInput);
 }
