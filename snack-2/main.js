76e558550d1dcba99e00b0af1421088ac318916f
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-06 09:41:31 +0200

    edit initialization to push

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,7 +4,8 @@ for (let i = 0; i < num; i++) {
   arr[i] = [];
 
   for (let j = 0; j < 10; j++) {
-    arr[i][j] = Math.floor(Math.random() * 100) + 1;
+    const rand = Math.floor(Math.random() * 100) + 1;
+    arr[i].push(rand);
   }
 
   console.log(`Array n${i + 1}: [${arr[i]}]`);
